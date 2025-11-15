106c244caa2a3b6aedcfbf758342e94f28a8b857
Sandip Duley <sandipp0987rahul@gmail.com>
2025-11-15 15:59:13 +0530

    02_warlus_operator

---
+++ 02_warlus_operator.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main(){
+	fruit := "apple"
+	price := 11.22
+	quantity := 33
+	isTasty := true
+
+	fmt.Printf("%q, %.2f, %v, %v", fruit, price, quantity, isTasty)
+}
