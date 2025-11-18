8cb7298a077031da5c281bbc8a4b6d6ca82f54eb
sandipduley <sandipp0987rahul@gmail.com>
2025-11-18 18:51:00 +0530

    done comparision operators

---
+++ 06_comparision-operators/01_comparision.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(2 > 2) // false
+	fmt.Println(2 < 2) // false
+	fmt.Println(2 >= 2) // true
+	fmt.Println(2 <= 2) // true
+	fmt.Println(2 == 2) // true
+	fmt.Println(2 != 2) // false
+}
