0bd7cacb3ca64eb592c1318b50365822ad5d42f8
sandipduley <sandipp0987rahul@gmail.com>
2025-11-19 19:32:11 +0530

    done logical operator

---
+++ 07_logical_operators/01_AND.go
@@ -0,0 +1,9 @@
+package main
+
+import ("fmt")
+
+func main() {
+	
+	fmt.Println(true && true)  // both condition should be true
+	fmt.Println(11 == 11 && 22 == 22)
+}
---
+++ 07_logical_operators/03_NOT.go
@@ -0,0 +1,9 @@
+package main
+
+import ("fmt")
+
+func main() {
+	isAdmin := false
+
+	fmt.Println(!isAdmin)  // flip the condition  --  true = false , false = true
+}
